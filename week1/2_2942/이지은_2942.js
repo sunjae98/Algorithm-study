db9df574af3d4c8a683c4e9451382d5ae8465445
egg-silver <v6984@naver.com>
2023-12-31 17:59:10 +0900

    1주차 897번 문제풀이

---
+++ week1/2_2942/이지은_2942.js
@@ -0,0 +1,9 @@
+var findWordsContaining = function (words, x) {
+  let arr = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].includes(x)) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
