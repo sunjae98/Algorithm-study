19d771f0366d21503e22b2f8b56cde7be79b9b62
gmlrude <qkrgmlrud00@gmail.com>
2024-01-14 17:23:18 +0900

    3주차 101번 문제풀이

---
+++ week3/6_101/박희경_101.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+
+        return dfs(root.left, root.right);
+    }
+    boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+}
