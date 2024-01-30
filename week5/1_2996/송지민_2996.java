eb50683ec7344a13c2038c00e149ed220724f04d
jiminsong490 <misosjm@naver.com>
2024-01-30 17:58:18 +0900

    5주차 2996번 문제풀이

--- week5/1_2996/송지민_2996.java
+++ week5/1_2996/송지민_2996.java
@@ -1,12 +1,17 @@
 class Solution {
-    public int minChanges(String s) {
-        int N = s.length();
-        int result = 0;
-
-        for(int i = 1; i < N; i+=2) {
-            if(s.charAt(i) != s.charAt(i-1)) result++;
+    public int missingInteger(int[] nums) {
+        int countSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 연속된 순열인지 확인한 후 합을 구함
+            if (nums[i - 1] + 1 == nums[i]) countSum += nums[i];
+            else break;
         }
-
-        return result;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (countSum == nums[i]) {
+                countSum++;
+            }
+        }
+        return countSum;
     }
 }
