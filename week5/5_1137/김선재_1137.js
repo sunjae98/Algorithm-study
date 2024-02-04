dfb9cc60af0c374a6d125f79fbfb772e2d85e443
kimsunjae <woal9844@gmail.com>
2024-02-04 21:48:04 +0900

    5주차 1137번 문제풀이

---
+++ week5/5_1137/김선재_1137.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function (n) {
+  let dp = [];
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 1;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+  }
+
+  return dp[n];
+};
