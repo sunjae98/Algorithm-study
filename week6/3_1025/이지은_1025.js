23c310e7cff62ebe135c9da922a125768e721230
egg-silver <v6984@naver.com>
2024-02-04 17:26:58 +0900

    6주차 1025번 문제 풀이

---
+++ week6/3_1025/이지은_1025.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function (n) {
+  // 앨리스는 n이 짝수일 때만 이김
+  return n % 2 === 0;
+};
