8108a5eb31226289412328f3ad0ce5568dc8cd6e
Heekyeong Park <qkrgmlrud00@gmail.com>
2024-02-04 21:55:09 +0900

    6주차 2914번 문제풀이

---
+++ week6/1_2914/박희경_2914.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minChanges(String s) {
+        int changes = 0;
+
+        for (int i = 1; i < s.length() ; i+=2) {
+            if(s.charAt(i) != s.charAt(i-1)) {
+                changes++;
+            }
+        }
+        return changes;
+    }
+}
