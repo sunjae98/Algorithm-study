c2078c83f67b7b964d554d31c8a6db78c9ec5f3b
woohee <aldkgha@naver.com>
2024-03-10 05:17:42 +0900

    11주차 92334 문제풀이

---
+++ week11/4_92334/정우희_92334.js
@@ -0,0 +1,31 @@
+function solution(id_list, report, k) {
+  let reportObj = new Map();
+  id_list.forEach((val) => {
+    return (reportObj[val] = {
+      report: [],
+      reported: 0,
+      isStoppedId: false,
+    });
+  });
+
+  report.forEach((val) => {
+    let [reportPerson, reportedPerson] = val.split(' ');
+
+    if (reportObj[reportPerson].report.indexOf(reportedPerson) === -1) {
+      reportObj[reportPerson].report.push(reportedPerson);
+      reportObj[reportedPerson].reported += 1;
+
+      if (reportObj[reportedPerson].reported >= k) {
+        reportObj[reportedPerson].isStoppedId = true;
+      }
+    }
+  });
+
+  let cntOfSendMail = id_list.map((val) => {
+    return reportObj[val].report.filter(
+      (reportId) => reportObj[reportId].isStoppedId
+    ).length;
+  });
+
+  return cntOfSendMail;
+}
