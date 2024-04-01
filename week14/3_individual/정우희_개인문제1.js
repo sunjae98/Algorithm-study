0e0775ddc60697f54698ba877623caaa28fa6a81
Woohee Jeong <121246589+Joy0w0@users.noreply.github.com>
2024-04-01 10:56:16 +0900

    Revert "[정우희] 14주차 과제 제출"

--- week14/3_individual/정우희_개인문제1.js
+++
@@ -1,8 +0,0 @@
-var isUnivalTree = function (root) {
-  if (!root) return true;
-
-  if (root.left && root.left.val !== root.val) return false;
-  if (root.right && root.right.val !== root.val) return false;
-
-  return isUnivalTree(root.left) && isUnivalTree(root.right);
-};
