16857e9d733c17aea1467f614cf8814186aa1bb7
jiminsong490 <songjimin@Jimin-MacMini.local>
2024-05-25 00:09:07 +0900

    22주차 6603번 문제풀이

---
+++ week22/1_6603/송지민_6603.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int k;
+    static int [] s;
+    static boolean [] chk;
+    public static void main(String[] args) throws  Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            String testCase = br.readLine();
+            if(testCase.equals("0")) break;
+            String [] input = testCase.split(" ");
+            k=Integer.parseInt(input[0]);
+            s=new int[k];
+            chk=new boolean[k];
+            for(int i=0;i<k;i++){
+                s[i]=Integer.parseInt(input[i+1]);
+            }
+
+            dfs(0,0);
+            System.out.println();
+        }
+    }
+
+    public static void dfs(int depth,int start){
+        if(depth==6){
+            for(int i=0;i<k;i++){
+                if(chk[i]){
+                    System.out.print(s[i]+" ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=start;i<k;i++){
+            chk[i]=true;
+            dfs(depth+1,i+1);
+            chk[i]=false;
+        }
+    }
+}
