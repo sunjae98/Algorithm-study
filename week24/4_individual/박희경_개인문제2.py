9556ef8f256140677f1f32b15dca1442ebf20322
gmlrude <qkrgmlrud00@gmail.com>
2024-06-09 18:00:15 +0900

    24주차 개인문제2 문제풀이

---
+++ week24/4_individual/박희경_개인문제2.py
@@ -0,0 +1,11 @@
+"""
+프로그래머스: 42842번 카펫
+"""
+def solution(brown, yellow):
+    grid = brown + yellow
+    for i in range(3, int(grid ** 0.5) + 1):
+        if grid % i != 0:
+            continue
+        j = grid // i
+        if (i-2) * (j-2) == yellow:
+            return [j, i]
