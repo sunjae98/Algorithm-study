85d38f9a6101b797ddede70ae2124f031a7b9455
egg-silver <v6984@naver.com>
2024-07-02 23:19:41 +0900

    라이브 코테 1번 문제

--- test1/문제1/이지은.js
+++ test1/문제1/이지은.js
@@ -1,8 +1,23 @@
 function solution(wallpaper) {
-  var answer = [];
-  arr = Array.from(Array(wallpaper.length), () =>
-    Array(wallpaper[0].length).fill(0)
-  );
+  let x = [];
+  let y = [];
+
+  for (let i = 0; i < wallpaper.length; i++) {
+    for (let j = 0; j < wallpaper[0].length; j++) {
+      if (wallpaper[i][j] === '#') {
+        x.push(i);
+        y.push(j);
+      }
+    }
+  }
+
+  var answer = [
+    Math.min(...x),
+    Math.min(...y),
+    Math.max(...x) + 1,
+    Math.max(...y) + 1,
+  ];
+
   return answer;
 }
 
